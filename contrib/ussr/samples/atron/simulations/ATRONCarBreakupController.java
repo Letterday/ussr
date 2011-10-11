894f7d24d615ee167a7751a0c2123f5f1493823d
Ulrik Pagh Schultz <ups@mmmi.sdu.dk>
2011-10-11 09:02:10 +0200

    Test of breakup behavior

---
+++ contrib/ussr/samples/atron/simulations/ATRONCarBreakupController.java
@@ -0,0 +1,73 @@
+/**
+ * Unified Simulator for Self-Reconfigurable Robots (USSR)
+ * (C) University of Southern Denmark 2008
+ * This software is distributed under the BSD open-source license.
+ * For licensing see the file LICENCE.txt included in the root of the USSR distribution.
+ */
+package ussr.samples.atron.simulations;
+
+import java.awt.Color;
+import java.util.List;
+
+import com.jme.math.Matrix3f;
+import com.jmex.physics.DynamicPhysicsNode;
+
+import ussr.comm.Packet;
+import ussr.comm.Receiver;
+import ussr.comm.Transmitter;
+import ussr.model.Sensor;
+import ussr.physics.jme.JMEModuleComponent;
+import ussr.samples.GenericSimulation;
+import ussr.samples.atron.ATRONController;
+
+/**
+ * A simple controller for an ATRON car that reports data from the proximity sensors
+ * 
+ * @author Modular Robots @ MMMI
+ *
+ */
+public class ATRONCarBreakupController extends ATRONController {
+	
+    /**
+     * @see ussr.model.ControllerImpl#activate()
+     */
+    public void activate() {
+    	setup(); 
+    	this.delay(1000); /* rotateContinuous seem to fail sometimes if we do not wait at first */
+        byte dir = 1;
+        float lastProx = Float.NEGATIVE_INFINITY; /* for printing out proximity data */
+        boolean firstTime = true;
+        while(true) {
+        	
+            // Basic control: first time we enter the loop start rotating and turn the axle
+            String name = module.getProperty("name");
+            if(firstTime) {
+                firstTime = false;
+                if(name.startsWith("wheel1")) rotateContinuous(dir);
+                if(name.startsWith("wheel2")) rotateContinuous(-dir);
+                if(name.startsWith("wheel3")) rotateContinuous(dir);
+                if(name.startsWith("wheel4")) rotateContinuous(-dir);
+                if(name.equals("axleOne5")) {
+                    this.rotateDegrees(10);
+                }
+                if(name.startsWith("driver")) disconnectAll();
+            }
+
+            // Print out proximity information
+            float max_prox = Float.NEGATIVE_INFINITY;
+            for(Sensor s: module.getSensors()) {
+                if(s.getName().startsWith("Proximity")) {
+                    float v = s.readValue();
+                    max_prox = Math.max(max_prox, v);
+                }
+            }
+            if(name.startsWith("wheel")&&Math.abs(lastProx-max_prox)>0.01) {
+                System.out.println("Proximity "+name+" max = "+max_prox);
+                lastProx = max_prox; 
+            }
+
+            // Always call yield sometimes
+        	yield();
+        }
+    }
+}
